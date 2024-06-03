30ea55c9b181598d19e66ee88a8a3d3ec38563ce
Eric Wise <eric.wise@thespurgroup.com>
2024-06-03 15:22:18 -0700

    Move Copilot icon to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,8 @@ function App() {
       </Modal>
       <div className='container'>
 
-        <iframe style={{ width: "100%", height: "100%" }} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FvFpGreQeOMqJpyB5U5v9VK%2FPartner-Basecamp%3Fpage-id%3D1659%253A207085%26node-id%3D1659-207124%26viewport%3D1629%252C603%252C0.12%26t%3DpHGeGGAzK5x0QEUw-1%26scaling%3Dcontain%26starting-point-node-id%3D1659%253A207086" allowfullscreen></iframe>
-        <button className='copilot-button' onClick={openModal}><img src="/src/assets/copilot-icon-small.png" /></button>
+        <iframe style={{ width: "100%", height: "100%" }} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FvFpGreQeOMqJpyB5U5v9VK%2FPartner-Basecamp%3Fpage-id%3D1659%253A207085%26node-id%3D1659-207124%26viewport%3D1629%252C603%252C0.12%26t%3DpHGeGGAzK5x0QEUw-1%26scaling%3Dcontain%26starting-point-node-id%3D1659%253A207086"></iframe>
+        <button className='copilot-button' onClick={openModal}><img src="/copilot-icon-small.png" /></button>
       </div>
 
     </>
