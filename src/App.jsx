9757fef4cf507180b25f87e0bafb2172b6a2bbca
Eric Wise <eric.wise@thespurgroup.com>
2024-06-03 13:57:34 -0700

    Add Copilot icon in bottom right of page, add modal with bot when Copilot icon clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
 import './App.css'
+import React, { useState } from 'react'
+import Modal from 'react-modal'
 
 function App() {
 
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
   return (
     <>
+      <Modal
+        isOpen={isOpen}
+        centered
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+        style={{
+          content: {
+            width: '800px', // or any desired width
+            height: '500px', // or any desired height
+          }
+        }}
+      >
+        <iframe src="https://web.powerva.microsoft.com/environments/Default-b00367e2-193a-4f48-94de-7245d45c0947/bots/cr75d_partnerAi/webchat?__version__=2" frameBorder="0" style={{ width: "800px", height: "500px" }}></iframe>
+        <button onClick={closeModal}>Close Modal</button>
+      </Modal>
+      <div className='container'>
 
-      <iframe style={{ width: "100%", height: "100%" }} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FvFpGreQeOMqJpyB5U5v9VK%2FPartner-Basecamp%3Fpage-id%3D1659%253A207085%26node-id%3D1659-207124%26viewport%3D1629%252C603%252C0.12%26t%3DpHGeGGAzK5x0QEUw-1%26scaling%3Dcontain%26starting-point-node-id%3D1659%253A207086" allowfullscreen></iframe>
+        <iframe style={{ width: "100%", height: "100%" }} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FvFpGreQeOMqJpyB5U5v9VK%2FPartner-Basecamp%3Fpage-id%3D1659%253A207085%26node-id%3D1659-207124%26viewport%3D1629%252C603%252C0.12%26t%3DpHGeGGAzK5x0QEUw-1%26scaling%3Dcontain%26starting-point-node-id%3D1659%253A207086" allowfullscreen></iframe>
+        <button className='copilot-button' onClick={openModal}><img src="src/assets/copilot-icon-small.png" /></button>
+      </div>
 
     </>
   )
