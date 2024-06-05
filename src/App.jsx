74d2c5cebca82575194c733b6d4bd307b3b61c4f
Eric Wise <eric.wise@thespurgroup.com>
2024-06-05 13:18:16 -0700

    Update copilot icon location, improve modal styling

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,15 @@ function App() {
           content: {
             width: '800px',
             height: '500px',
-            inset: '20% 30%',
+            inset: '10% 20%',
+            overflow: 'hidden',
+            border: '2px solid rgb(11, 85, 106)',
           }
         }}
       >
         <iframe src="https://web.powerva.microsoft.com/environments/Default-b00367e2-193a-4f48-94de-7245d45c0947/bots/cr75d_partnerAi/webchat?__version__=2" frameBorder="0" style={{ width: "800px", height: "500px" }}></iframe>
-        <button onClick={closeModal}>Close Modal</button>
       </Modal>
       <div className='container'>
-
         <iframe style={{ position: "fixed", left: "0", top: "0", width: "100%", height: "100%", margin: "0px", padding: "0px", border: "none" }} width="100%" height="100%" src="
 https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FvFpGreQeOMqJpyB5U5v9VK%2FPartner-Basecamp%3Fpage-id%3D1659%253A207085%26node-id%3D1659-207086%26viewport%3D1629%252C603%252C0.12%26t%3DkjtjyjuJDP9HD16S-1%26scaling%3Dscale-down-width%26starting-point-node-id%3D1659%253A207086%26content-scaling%3Dfixed%26hide-ui%3D1"
           allowfullscreen></iframe>
